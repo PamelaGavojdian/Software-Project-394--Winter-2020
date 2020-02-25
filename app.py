ff82c269f91d36f02c0e5a3e77f9746d7a334e98
Jake Janicke <37964537+JakeTripplJ@users.noreply.github.com>
2020-02-24 18:34:37 -0600

    Updated Flask + HTML
    Fixed HTML references to use templates, added dummy Job array

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from custom.objects.Job import Job
 import json
 
 
@@ -7,14 +8,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
 
-@app.route("/ReturningJson")
-def returnJson():
-	return json.dumps([1, 2, 3, {'4': 5, '6': 7}])
+	testJobList = [
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago"),
+		Job(position="Test1", salary = 100 , location="Chicago"),
+		Job(position="Test2", salary = 200 , location="Chicago"), 
+		Job(position="Test3", salary = 300 , location="Chicago")
+		]
+
+
+	return render_template("jobList.html", jobs = testJobList)
 
 if __name__ == "__main__":
 	app.run()
-
 
 ##Job Is a callable Object now
